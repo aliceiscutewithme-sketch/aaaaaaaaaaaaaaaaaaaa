3df32a508b2c2b00c1a59a1dce43b9859050b07c
aliceiscutewithme-sketch <aliceiscutewithme@gmail.com>
2025-12-28 08:34:48 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,8 +1,8 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1446774779150340119",
-        serverId: "1446602120441757930",
+        channelId: "547892148809498634",
+        serverId: "224308865427046402",
         token: process.env.token1,
         selfDeaf: true,
         autoReconnect: {
